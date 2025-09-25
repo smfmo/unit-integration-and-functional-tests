58166927a0b2330160a2d8c30e4e58fcd1706036
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-25 12:36:23 -0300

    test: Implementar teste no método de calcular aluguel
    - Adiciona método simples que calcula o valor do aluguel, multiplicando o valor da diária pela quantidade de dias,
    aonde supondo que o valor do aluguel seja de R$ 100.00 o resultado esperado para 3 dias seja de R$ 300.00.

--- src/test/java/io/github/smfmo/locadora/model/CarTest.java
+++ src/test/java/io/github/smfmo/locadora/model/CarTest.java
@@ -1,5 +1,18 @@
 package io.github.smfmo.locadora.model;
 
-public class CarTest {
-    
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    @DisplayName("Este método deve calcular o valor do aluguel, multiplicando o valor da diária pelo total de dias.")
+    void mustCalculateRentalValue() {
+        Car car = new Car("Sedan", 100.0);
+        double total = car.calculateRentValue(3);
+
+        Assertions.assertEquals(300.0, total);
+    }
+
 }
