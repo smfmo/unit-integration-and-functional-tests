90a280f45a36f4c82521e29f224daf9d247dc98e
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-29 16:46:08 -0300

    feat: Criar Entidade Client
    - implementa entidade Client na aplicação com apenas getters/setters e contrutores

---
+++ src/main/java/io/github/smfmo/locadora/model/Client.java
@@ -0,0 +1,19 @@
+package io.github.smfmo.locadora.model;
+
+public class Client {
+
+    private String name;
+
+    public Client() {}
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
