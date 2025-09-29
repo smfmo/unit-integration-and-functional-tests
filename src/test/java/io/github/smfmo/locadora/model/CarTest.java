4141bc2108e54d3ce9d513e3462f82a3490e618d
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-29 14:40:24 -0300

    test: Implementar teste que valida o calculo do valor do aluguel de 5 dias para cima
    - cria método de teste mustCalculateDiscountRentalValue onde deve calcular o valor do aluguel com desconto
    (Acima de 5 dias = desconto de R$ 50.00).
    - Adiciona comentários que ajudam a documentar melhor as 3 partes de um teste (Arrange, Act, Assert) = 3As.

--- src/test/java/io/github/smfmo/locadora/model/CarTest.java
+++ src/test/java/io/github/smfmo/locadora/model/CarTest.java
@@ -9,10 +9,22 @@ class CarTest {
     @Test
     @DisplayName("Este método deve calcular o valor do aluguel, multiplicando o valor da diária pelo total de dias.")
     void mustCalculateRentalValue() {
+        // 1. Scenario = Arrange
         Car car = new Car("Sedan", 100.0);
+        // 2. Execution = Act
         double total = car.calculateRentValue(3);
-
+        // 3. Verification = Assert
         Assertions.assertEquals(300.0, total);
     }
 
+    @Test
+    @DisplayName("Este método deve calcular o valor do aluguel com desconto. (Acima de 5 dias = desconto de R$ 50.00)")
+    void mustCalculateDiscountedRentalValue() {
+        Car car = new Car("Sedan", 100.0);
+        int quantityDays = 5;
+
+        double total = car.calculateRentValue(quantityDays);
+
+        Assertions.assertEquals(450.0, total);
+    }
 }
