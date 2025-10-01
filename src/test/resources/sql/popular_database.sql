3186aff3cb2fff88eb01feb5ba1ebcaa63e4d480
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-01 10:56:47 -0300

    test: Adicionar arquivo sql com inserts para popular os testes com dados.

---
+++ src/test/resources/sql/popular_database.sql
@@ -0,0 +1,2 @@
+INSERT INTO car (id, model, daily_value) values ('974d5e85-2636-450c-8403-d8d81c3126b3', 'SUV', 150.0);
+INSERT INTO car (id, model, daily_value) values ('fdb165be-2866-4b11-84ca-c0d9c5241684','Hatch', 80.0);
