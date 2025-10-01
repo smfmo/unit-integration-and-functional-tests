b4f3c612d66e2d1681e69261ddc7c326ef032a6b
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-30 22:26:37 -0300

    feat: criar entidade Car para realizar testes de persistência

---
+++ src/main/java/io/github/smfmo/locadora/entity/CarEntity.java
@@ -0,0 +1,46 @@
+package io.github.smfmo.locadora.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "car")
+public class CarEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "daily_value")
+    private Double dailyValue;
+
+    public CarEntity() {}
+
+    public CarEntity(String model, Double dailyValue) {
+        this.model = model;
+        this.dailyValue = dailyValue;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public Double getDailyValue() {
+        return dailyValue;
+    }
+    public void setDailyValue(Double dailyValue) {
+        this.dailyValue = dailyValue;
+    }
+}
