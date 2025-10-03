22b2e012909137df837ab0bf2aab9653fb5f33bc
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-03 10:53:22 -0300

    test: Adicionar campo 'year' nos scripts do banco para os testes

--- src/test/resources/sql/popular_database.sql
+++ src/test/resources/sql/popular_database.sql
@@ -1,2 +1,5 @@
-INSERT INTO car (id, model, daily_value) values ('974d5e85-2636-450c-8403-d8d81c3126b3', 'SUV', 150.0);
-INSERT INTO car (id, model, daily_value) values ('fdb165be-2866-4b11-84ca-c0d9c5241684','Hatch', 80.0);
+INSERT INTO car (id, model, daily_value, year)
+values ('974d5e85-2636-450c-8403-d8d81c3126b3', 'SUV', 150.0, 2015);
+
+INSERT INTO car (id, model, daily_value, year)
+values ('fdb165be-2866-4b11-84ca-c0d9c5241684','Hatch', 80.0, 2018);
