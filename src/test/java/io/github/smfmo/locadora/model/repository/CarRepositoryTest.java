5f2ccc27a117fcf309ca33c736a10efaebda9c79
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-03 11:16:38 -0300

    refactor: organização nas importações

--- src/test/java/io/github/smfmo/locadora/model/repository/CarRepositoryTest.java
+++ src/test/java/io/github/smfmo/locadora/model/repository/CarRepositoryTest.java
@@ -8,9 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
