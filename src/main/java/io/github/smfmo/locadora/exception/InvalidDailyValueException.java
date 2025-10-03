64011ff6d7d391170486685e881e0df134b754b8
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-03 12:36:43 -0300

    feat (exception): Implementar InvalidDailyValueException para validar os valores da diária do carro

---
+++ src/main/java/io/github/smfmo/locadora/exception/InvalidDailyValueException.java
@@ -0,0 +1,7 @@
+package io.github.smfmo.locadora.exception;
+
+public class InvalidDailyValueException extends RuntimeException {
+    public InvalidDailyValueException(String message) {
+        super(message);
+    }
+}
